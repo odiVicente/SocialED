febad910a1c8908a31198d6ca2c30a9c7fc90351
Hector Cano <hectorcanomaestre@hotmail.com>
2021-05-10 19:31:15 +0200

    funcionando

--- server.py
+++ server.py
@@ -1,28 +1,29 @@
 # -*- coding: iso-8859-15 -*-
 
-
+from flask import render_template
 from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return app.send_static_file('index.html')
+    return render_template("index.html")
 
 
 @app.route('/home', methods=['GET'])
 def home():
-    return app.send_static_file('home.html')
+    return render_template("home.html")
 
 
 @app.route('/login', methods=['GET'])
 def login():
-    return app.send_static_file('login.html')
+    return render_template("login.html")
+
 
 
 @app.route('/signup', methods=['GET'])
 def signup():
-    return app.send_static_file('signup.html')
+    return render_template("signup.html")
 
 
 @app.route('/processLogin', methods=['GET', 'POST'])
@@ -37,20 +38,7 @@ def processLogin():
               return "Warning: Some fields are missing"
 
 
-       return render_template("login.html") 
-       #'<!DOCTYPE html> ' \
-           #'<html lang="es">' \
-           #'<head>' \
-           #'<title> Home - SocNet </title>' \
-           # '<link href="static/css/socialed-style.css" rel="stylesheet" type="text/css"/>'\
-           #'</head>' \
-           #'<body> <div id ="container">' \
-		   #'<a href="/"> SocNet </a> | <a href="home"> Home </a> | <a href="login"> Log In </a> | <a href="signup"> Sign Up </a>' \
-           #'<h1>Data from Form: Login</h1>' \
-	       #'<form><label>email: ' + request.form['email'] + \
-	       #'</label><br><label>passwd: ' + request.form['passwd'] + \
-           #'</label></form></div></body>' \
-           #'</html>'
+       return render_template("login.html")
 
 
 @app.route('/processSignup', methods=['GET', 'POST'])
@@ -64,23 +52,7 @@ def processSignup():
        if missing:
               return "Warning: Some fields are missing"
 
-       return render_template("singup.html")
-
-        #    '<!DOCTYPE html> ' \
-        #   '<html lang="es">' \
-        #   '<head>' \
-        #   '<title> Inicio - SocialED </title>' \
-        #    '<link href="static/css/socialed-style.css" rel="stylesheet" type="text/css"/>'\
-        #   '</head>' \
-        #   '<body> <div id ="container">' \
-		#   '<a href="/"> SocialED </a> | <a href="home"> Home </a> | <a href="login"> Log In </a> | <a href="signup"> Sign Up </a>' \
-        #   '<h1>Data from Form: Sign Up</h1>' \
-        #   '<form><label>Nickame: ' + request.form['nickname'] + \
-	    #   '</label><br><label>email: ' + request.form['email'] + \
-	    #   '</label><br><label>passwd: ' + request.form['passwd'] + \
-	     #  '</label><br><label>confirm: ' + request.form['confirm'] + \
-          # '</label></form></div></body>' \
-          # '</html>'
+       return render_template("signup.html")
 
 
 @app.route('/processHome', methods=['GET', 'POST'])
@@ -95,27 +67,7 @@ def processHome():
 		return "Warning: Some fields are missing"
 
 	return render_template("home.html")
-    #'<!DOCTYPE html> ' \
-     #      '<html lang="es">' \
-      #     '<head>' \
-      #     '<title> Inicio - SocialED </title>' \
-      #      '<link href="static/css/socialed-style.css" rel="stylesheet" type="text/css"/>'\
-      #     '</head>' \
-      #     '<body> <div id="container">' \
-	#	   '<a href="/"> SocialED </a> | <a href="home"> Home </a> | <a href="login"> Log In </a> | <a href="signup"> Sign Up </a>' \
-       #    '<h1>Hi, How are you?</h1>' \
-       #         	'<form action="processHome" method="post" name="home"> ' \
-		#	'<label for="message">Say something:</label><div class="inputs">' \
-		#	'<input id="message" maxlength="128" name="message" size="80" type="text" required="true" value=""/>' \
-		#	'<input id="last" type="hidden" name="last" required="true" value="' + request.form['last'] + '<br>'+ request.form['message'] + '">' \
-	    #             '</div>' \
-        #            	'<div class="inputs">' \
-        #                '<input id="post_submit" name="post_submit" type="submit" value="Post!"/>' \
-        #   		'<br><br>Previous Posts: <br>' + request.form['last'] + '<br>' +request.form['message'] + \
-        #        	'</form>' \
-        #    		'</div></div>' \
-        #   '</body>' \
-        #   '</html>'''
+
 
 
 #app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
