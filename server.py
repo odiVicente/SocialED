4f8311620a0eae900abd86a62410d15b93100694
VicenteA <vicantserr@icloud.com>
2021-05-13 23:11:04 +0200

    Las cookies funcionan

--- server.py
+++ server.py
@@ -9,68 +9,6 @@ import sys
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
-def index():
-    #return app.send_static_file('index.html')
-    return render_template("index.html")
-
-@app.route('/home', methods=['GET'])
-def home():
-    #return app.send_static_file('home.html')
-    return render_template("home.html")
-
-@app.route('/login', methods=['GET'])
-def login():
-    #return app.send_static_file('login.html')
-    return render_template("login.html")
-
-
-@app.route('/signup', methods=['GET'])
-def signup():
-    #return app.send_static_file('signup.html')
-    return render_template("signup.html")
-
-@app.route('/processLogin', methods=['GET', 'POST'])
-def processLogin():
-       missing = []
-       fields = ['email', 'passwd', 'login_submit']
-       for field in fields:
-              value = request.form.get(field, None)
-              if value is None:
-                  missing.append(field)
-       if missing:
-             return process_missingFields(missing, "/login")
-
-       return render_template("login.html", email = request.form['email'], passwd = request.form['passwd'] )
-
-
-@app.route('/processSignup', methods=['GET', 'POST'])
-def processSignup():
-       missing = []
-       fields = ['nickname', 'email', 'passwd','confirm', 'signup_submit']
-       for field in fields:
-              value = request.form.get(field, None)
-              if value is None:
-                     missing.append(field)
-       if missing:
-              return process_missingFields(missing, "/signup")
-
-       return render_template("signup.html", nickname = request.form['nickname'], email = request.form['email'], passwd = request.form['passwd'], confirm = request.form['confirm'] )
-
-
-@app.route('/processHome', methods=['GET', 'POST'])
-def processHome():
-	missing = []
-	fields = ['message', 'last', 'post_submit']
-	for field in fields:
-		value = request.form.get(field, None)
-		if value is None:
-			missing.append(field)
-	if missing:
-		return process_missingFields(missing, "/home")
-
-	return render_template("home.html", last = request.form['last'], message = request.form['message'])
 def load_user(email, passwd):
     """
     Carga datos usuario (identified by email) del directorio data.
@@ -81,11 +19,11 @@ def load_user(email, passwd):
     """
     file_path = os.path.join(SITE_ROOT, "data/", email)
     if not os.path.isfile(file_path):
-        return process_error("User not found / No existe un usuario con ese nombre", url_for("login.html"))
+        return process_error("User not found / No existe un usuario con ese nombre", url_for("login"))
     with open(file_path, 'r') as f:
         data = json.load(f)
     if data['password'] != passwd:
-        return process_error("Incorrect password / la clave no es correcta", url_for("login.html"))
+        return process_error("Incorrect password / la clave no es correcta", url_for("login"))
     session['user_name'] = data['user_name']
     session['messages'] = data['messages']
     session['password'] = passwd
@@ -94,6 +32,7 @@ def load_user(email, passwd):
     return redirect(url_for("home"))
 
 def save_current_user():
+    session['messages']=request.form['message']+request.form['last']
     datos = {
         "user_name": session["user_name"],
         "password": session['password'],
@@ -104,6 +43,7 @@ def save_current_user():
     file_path = os.path.join(SITE_ROOT, "data/", session['email'])
     with open(file_path, 'w') as f:
         json.dump(datos, f)
+    return render_template("home.html", last = session['messages'], message = request.form['message'])
 
 
 def create_user_file(name, email, passwd, passwd_confirmation):
@@ -152,6 +92,69 @@ def process_error(message, next_page):
 
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+@app.route('/', methods=['GET'])
+def index():
+    #return app.send_static_file('index.html')
+    return render_template("index.html")
+
+@app.route('/home', methods=['GET'])
+def home():
+    #return app.send_static_file('home.html')
+    return render_template("home.html")
+
+@app.route('/login', methods=['GET'])
+def login():
+    #return app.send_static_file('login.html')
+    return render_template("login.html")
+
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    #return app.send_static_file('signup.html')
+    return render_template("signup.html")
+
+@app.route('/processLogin', methods=['GET', 'POST'])
+def processLogin():
+        missing = []
+        fields = ['email', 'passwd', 'login_submit']
+        for field in fields:
+                value = request.form.get(field, None)
+                if value is None:
+                    missing.append(field)
+        if missing:
+            return process_missingFields(missing, "/login")
+
+        return load_user(request.form['email'],request.form['passwd'])
+
+
+@app.route('/processSignup', methods=['GET', 'POST'])
+def processSignup():
+        missing = []
+        fields = ['nickname', 'email', 'passwd','confirm', 'signup_submit']
+        for field in fields:
+                value = request.form.get(field, None)
+                if value is None:
+                    missing.append(field)
+        if missing:
+                return process_missingFields(missing, "/signup")
+
+        return create_user_file(request.form['nickname'], request.form['email'], request.form['passwd'], request.form['confirm'])
+        
+
+@app.route('/processHome', methods=['GET', 'POST'])
+def processHome():
+    missing = []
+    fields = ['message', 'last', 'post_submit']
+    for field in fields:
+	    value = request.form.get(field, None)
+	    if value is None:
+		    missing.append(field)
+    if missing:
+	    return process_missingFields(missing, "/home")
+    return save_current_user()
+	#return render_template("home.html", last = request.form['last'], message = request.form['message'])
+
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True, port=55550)
